b4179bc4d68bbf388a287a2520aa1687f1e80c9c
Guaroduino <guaroduino@gmail.com>
2025-05-22 12:29:19 -0400

    Actualizar configuración de Next.js y workflow para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: '/guaroduino-steam',
+  assetPrefix: '/guaroduino-steam/',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
